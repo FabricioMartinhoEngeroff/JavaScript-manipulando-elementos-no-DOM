c0430408205b343db8b9c2634b85c880ce05df05
Fabricio <fa.engeroff1996@gmail.com>
2024-11-02 17:05:50 -0300

    Iniciei a contagem regressiva automaticamente com setInterval(), exibindo o tempo decrescente no console. Adicionei a funcionalidade de pausar a contagem regressiva ao clicar no botão, permitindo controle sobre o temporizador. Integrei efeitos sonoros que tocam ao iniciar, pausar, e quando o tempo se esgota, tornando a experiência mais interativa.

--- app/buttons.js
+++ app/buttons.js
@@ -5,12 +5,20 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
+
+const beepAudio = new Audio('/sons/beep.mp3')
+const playAudio = new Audio('/sons/play.wav')
+const pauseAudio = new Audio('/sons/pause.mp3')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
 musica.loop = true
 
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
 musicaFocoInput.addEventListener('change', () => {
-    if(musica.paused) {
+    if (musica.paused) {
         musica.play()
     } else {
         musica.pause()
@@ -33,8 +41,8 @@ longoBt.addEventListener('click', () => {
 })
 
 function alterarContexto(contexto) {
-    botoes.forEach(function (contexto){
-        contexto.classList.remove('active')
+    botoes.forEach((botao) => {
+        botao.classList.remove('active')
     })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
@@ -42,19 +50,48 @@ function alterarContexto(contexto) {
         case "foco":
             titulo.innerHTML = `
             Otimize sua produtividade,<br>
-                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
             `
-            break;
+            break
         case "descanso-curto":
             titulo.innerHTML = `
             Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
-            ` 
-            break;
+            `
+            break
         case "descanso-longo":
             titulo.innerHTML = `
             Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
             `
+            break
         default:
-            break;
+            break
     }
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        zerar()
+        beepAudio.play()
+        alert('Tempo Finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador:' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        zerar()
+        pauseAudio.play()
+    } else {
+        intervaloId = setInterval(contagemRegressiva, 1000)
+        playAudio.play()
+    }
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
